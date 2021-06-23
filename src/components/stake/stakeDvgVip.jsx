af6c477a997c2db17143d123c8df3d0270686e33
Soon Lai <soonlai814@gmail.com>
2021-06-23 21:15:58 +0800

    updated

--- src/components/stake/stakeDvgVip.jsx
+++ src/components/stake/stakeDvgVip.jsx
@@ -866,7 +866,7 @@ class StakeDvgVip extends Component {
                                     </h3>
 
                                     <h4>
-                                        <a href="#" target="_blank" className={classes.seeMore}>See more here.</a>
+                                        <a href="https://daoventuresco.medium.com/" target="_blank" className={classes.seeMore}>See more here.</a>
                                     </h4>
                                    
                                     <ul>
