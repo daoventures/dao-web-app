bd06abc4f0d11c46604c1355861df71bfac58afe
Phyllice Wai <waikahkwan0903@gmail.com>
2021-06-24 15:55:01 +0800

    Update color code for error message.

--- src/components/stake/stakeDvgVip.jsx
+++ src/components/stake/stakeDvgVip.jsx
@@ -557,7 +557,7 @@ const styles = theme => ({
     },
 
     errorMessage: {
-        color: theme.themeColors.red,
+        color: theme.themeColors.formError,
         marginTop: "3px"
     }
 });
