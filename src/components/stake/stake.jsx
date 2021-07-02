b8639a1a3b58b30b1fa87c5fb13ef1604dac364b
Phyllice Wai <waikahkwan0903@gmail.com>
2021-07-02 09:54:12 +0800

    Add DAOmine APY display + Use constant "ALL" for risk level tab.

--- src/components/stake/stake.jsx
+++ src/components/stake/stake.jsx
@@ -19,7 +19,8 @@ import {
   DEPOSIT_DAOMINE_RETURNED_COMPLETED,
   WITHDRAW_DAOMINE_RETURNED,
   WITHDRAW_DAOMINE_RETURNED_COMPLETED,
-  ERROR
+  ERROR,
+  ALL
 } from "../../constants/constants";
 
 import RiskLevelTab from "../common/riskLevelTab/riskLevelTab";
@@ -300,7 +301,7 @@ class Stake extends Component {
           )
         : null,
       pools: store.getStore("stakePools"),
-      currentTab: "All",
+      currentTab: ALL,
       expanded: "",
       loading: false,
     };
@@ -553,7 +554,7 @@ class Stake extends Component {
     return pools && pools.length > 0
       ? pools
           .filter((pool) => {
-            return currentTab === "All" || pool.category === currentTab
+            return currentTab === ALL || pool.category === currentTab
               ? true
               : false;
           })
