10c78a7c0bd1f7ea89bacb24a4dfeda8005dfcbf
Phyllice Wai <waikahkwan0903@gmail.com>
2021-07-05 12:19:46 +0800

    Fix amount still in decimal even after being magnified issue. (DAOmine)

--- src/stores/store.jsx
+++ src/stores/store.jsx
@@ -6973,7 +6973,7 @@ class Store {
     const poolDecimal = pool.decimal;
     const poolIndex = pool.pid;
 
-    const amountToSend = (poolDecimal !== "18") ? web3.utils.toBN(amount * 10 ** poolDecimal).toString() : web3.utils.toWei(amount, "ether");
+    const amountToSend = (poolDecimal !== "18") ? web3.utils.toBN(Math.floor(amount * 10 ** poolDecimal)).toString() : web3.utils.toWei(amount, "ether");
 
     daoStakeContract.methods
       .deposit(poolIndex, amountToSend)
