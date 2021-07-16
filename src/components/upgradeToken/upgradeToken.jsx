d156694e9ca312237a3ad120da864bc3aa946818
Phyllice Wai <waikahkwan0903@gmail.com>
2021-07-16 21:49:03 +0800

    Update logo.

--- src/components/upgradeToken/upgradeToken.jsx
+++ src/components/upgradeToken/upgradeToken.jsx
@@ -601,7 +601,7 @@ class UpgradeToken extends Component {
             return (
                 <div className={classes.dvdBalanceRow}>
                     <div className={classes.balanceLeft}>
-                        <img className={classes.balanceLogo} src={require('../../assets/DAO-logo.png')} alt="" />
+                        <img className={classes.balanceLogo} src={require('../../assets/DAO-logo-2.png')} alt="" />
                     </div>
                     <div className={classes.balanceRight}>
                         <div className={classes.balanceAmount}>{dvdBalance
