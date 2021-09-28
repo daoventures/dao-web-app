469f785b92bb019c7feb6f78675e8d70a1b714b9
Phyllice Wai <waikahkwan0903@gmail.com>
2021-09-28 17:54:05 +0800

    Update page link on side drawer.

--- src/components/sideDrawer/sideDrawer.jsx
+++ src/components/sideDrawer/sideDrawer.jsx
@@ -321,7 +321,7 @@ class SideDrawer extends Component {
                 (store.getStore("account") && store.getStore("account").balance) || "",
             totalValue: "",
             listItem: [
-                {key: PORTFOLIO, name: "portfolio", path: PORTFOLIO_PATH, icon: "#iconmenu_porftfolio_nor_day"},
+                // {key: PORTFOLIO, name: "portfolio", path: PORTFOLIO_PATH, icon: "#iconmenu_porftfolio_nor_day"},
                 {
                     key: INVEST,
                     name: "invest",
@@ -340,12 +340,12 @@ class SideDrawer extends Component {
                         {key: "DAOvip (DVD)", name: "stake-dvd", path: STAKE_PATH_DVD},
                     ],
                 },
-                {
-                    key: SWAP,
-                    name: "swap",
-                    path: SWAP_PATH,
-                    icon: "#iconmenu_features_nor_night",
-                },
+                // {
+                //     key: SWAP,
+                //     name: "swap",
+                //     path: SWAP_PATH,
+                //     icon: "#iconmenu_features_nor_night",
+                // },
                 {
                     key: UPGPRADE,
                     name: "upgrade",
